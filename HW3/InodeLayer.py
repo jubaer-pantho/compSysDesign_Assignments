60538bd2a6ae7deaa469257e48f9f335b7e6e8f0
jubaer-pantho <jubaer.pantho@gmail.com>
2019-09-25 22:12:21 -0400

    Add HW3 initial files

---
+++ HW3/InodeLayer.py
@@ -0,0 +1,45 @@
+'''
+THIS MODULE IS INODE LAYER OF THE FILE SYSTEM. IT INCLUDES THE INODE DEFINITION DECLARATION AND GLOBAL HANDLE OF BLOCK LAYER OF API.
+THIS MODULE IS RESPONSIBLE FOR PROVIDING ACTUAL BLOCK NUMBERS SAVED IN INODE ARRAY OF BLOCK NUMBERS TO FETCH DATA FROM BLOCK LAYER.
+'''
+import datetime, config, BlockLayer, InodeOps
+
+#HANDLE OF BLOCK LAYER
+interface = BlockLayer.BlockLayer()
+
+class InodeLayer():
+
+    #RETURNS BLOCK NUMBER FROM RESPECTIVE INODE DIRECTORY
+    def INDEX_TO_BLOCK_NUMBER(self, inode, index):
+        if index == len(inode.blk_numbers): return -1
+        return inode.blk_numbers[index]
+
+
+    #RETURNS BLOCK DATA FROM INODE
+    def INODE_TO_BLOCK(self, inode, offset):
+        index = offset / config.BLOCK_SIZE
+        block_number = self.INDEX_TO_BLOCK_NUMBER(inode, index)
+        if block_number == -1: return ''
+        else: return interface.BLOCK_NUMBER_TO_DATA_BLOCK(block_number)
+
+
+    #MAKES NEW INODE OBJECT
+    def new_inode(self, type):
+        return InodeOps.Table_Inode(type)
+
+
+    #FLUSHES ALL THE BLOCKS OF INODES FROM GIVEN INDEX OF MAPPING ARRAY  
+    def free_data_block(self, inode, index):
+        for i in range(index, len(inode.blk_numbers)):
+            interface.free_data_block(inode.blk_numbers[i])
+            inode.blk_numbers[i] = -1
+
+
+    #IMPLEMENTS WRITE FUNCTIONALITY
+    def write(self, inode, offset, data):
+        #PLACE CODE FROM HW2 HERE
+
+    #IMPLEMENTS THE READ FUNCTION 
+    def read(self, inode, offset, length): 
+        #PLACE CODE FROM HW2 HERE
+
