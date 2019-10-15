7d619a58df51c161645d7a208781b99ed8727977
jubaer-pantho <jubaer.pantho@gmail.com>
2019-10-15 17:00:27 -0400

    Add functions to server_stub include pickle api

--- HW4/client_stub.py
+++ HW4/client_stub.py
@@ -21,23 +21,30 @@ class client_stub():
     ''' WRITE CODE HERE '''
     def inode_number_to_inode(self, inode_number):
         try :
-            self.proxy.inode_number_to_inode(inode_number)
+            serialVal = self.proxy.inode_number_to_inode(inode_number)
+            retVal = pickle.loads(serialVal)
+            return retVal
+
         except Exception as err :
-            # print error message
+            print("error message: inode_number_to_inode failed")
             quit()
 
     def get_data_block(self, block_number):
         try :
-            self.proxy.get_data_block(block_number)
+            serialVal = self.proxy.get_data_block(block_number)
+            retVal = pickle.loads(serialVal)
+            return retVal
         except Exception as err :
-            # print error message
+            print("error message: get_data_block failed")
             quit()
 
     def get_valid_data_block(self):
         try :
-            self.proxy.get_valid_data_block()
+            serialVal = self.proxy.get_valid_data_block()
+            retVal = pickle.loads(serialVal)
+            return retVal
         except Exception as err :
-            # print error message
+            print("error message: get_valid_data_block failed")
             quit()
 
 
@@ -45,27 +52,29 @@ class client_stub():
         try :
             self.proxy.free_data_block(block_number)
         except Exception as err :
-            # print error message
+            print("error message: free_data_block failed")
             quit()
 
     def update_data_block(self, block_number, block_data):
         try :
             self.proxy.update_data_block(block_number, block_data)
         except Exception as err :
-            # print error message
+            print("error message: update_data_block failed")
             quit()
 
     def update_inode_table(self, inode, inode_number):
         try :
             self.proxy.update_inode_table(inode, inode_number)
         except Exception as err :
-            # print error message
+            print("error message:update_inode_table failed")
             quit()
 
     def status(self):
         try :
-            self.status()
+            serialVal = self.proxy.status()
+            retVal = pickle.loads(serialVal)
+            return retVal
         except Exception as err :
-            # print error message
+            print("error message: status failed")
             quit()
 
--- HW4/server_stub.py
+++ HW4/server_stub.py
@@ -18,6 +18,46 @@ def Initialize():
 
 ''' WRITE CODE HERE FOR REST OF FUNCTIONS'''
 
+def inode_number_to_inode(inode_number):
+    retVal = Memory.inode_number_to_inode(inode_number)
+    retVal = pickle.dumps(retVal)
+    return retVal
+
+
+#REQUEST THE DATA FROM THE SERVER
+def get_data_block(block_number):
+    retVal = ''.join(Memory.get_data_block(block_number))
+    retVal = pickle.dumps(retVal)
+    return retVal
+
+
+#REQUESTS THE VALID BLOCK NUMBER FROM THE SERVER 
+def get_valid_data_block():
+    retVal = ( Memory.get_valid_data_block() )
+    retVal = pickle.dumps(retVal)
+    return retVal
+
+
+#REQUEST TO MAKE BLOCKS RESUABLE AGAIN FROM SERVER
+def free_data_block(block_number):
+    Memory.free_data_block((block_number))
+
+
+#REQUEST TO WRITE DATA ON THE THE SERVER
+def update_data_block(block_number, block_data):
+    Memory.update_data_block(block_number, block_data)
+
+
+#REQUEST TO UPDATE THE UPDATED INODE IN THE INODE TABLE FROM SERVER
+def update_inode_table(inode, inode_number):
+    Memory.update_inode_table(inode, inode_number)
+
+
+#REQUEST FOR THE STATUS OF FILE SYSTEM FROM SERVER
+def status():
+    retVal = Memory.status()
+
+
 
 
 server = SimpleXMLRPCServer(("",8000))
@@ -28,6 +68,13 @@ print ("Listening on port 8000...")
 #example provided for initialize
 server.register_function(Initialize,                "Initialize")
 
+server.register_function(inode_number_to_inode,     "inode_number_to_inode")
+server.register_function(get_data_block,            "get_data_block")
+server.register_function(get_valid_data_block,      "get_valid_data_block")
+server.register_function(free_data_block,           "free_data_block")
+server.register_function(update_data_block,         "update_data_block")
+server.register_function(update_inode_table,        "update_inode_table")
+server.register_function(status,                    "status")
 ''' WRITE CODE HERE FOR REST OF REGISTER CALLS '''
 
 # run the server
